9efd088420af2251156f4b2c5a6211ddf012cf6e
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2022-03-11 01:42:40 -0300

    up changes

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api 
 
 app = Flask(__name__)
 api = Api(app)
 
+
+items = []
+
 class Student(Resource):
     def get(self, name):
         return {'student': name}
 
+class Item(Resource):
+    def get(self, name):
+        print(items)
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self,name):
+        data = request.get_json()
+        items.append({'name': data['name'], 'price': data['price']})
+        return data, 201
 
 api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
